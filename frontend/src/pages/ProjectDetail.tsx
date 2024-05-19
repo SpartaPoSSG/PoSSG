d80a661bb3c2ef2f74f0af20c51f530b2772e53d
jiheunkim <ziheun54@gmail.com>
2024-05-19 20:41:03 +0900

    Design: 업로드 영역 내 Logo 하나로 통일

--- frontend/src/pages/ProjectDetail.tsx
+++ frontend/src/pages/ProjectDetail.tsx
@@ -20,16 +20,6 @@ const ProjectDetail = () => {
   const popupRef = useRef<HTMLDivElement>(null);
   
   const Logo = () => (
-    <svg className="w-24 h-24 mt-5 pointer-events-none" x="0px" y="0px" viewBox="0 0 24 24">
-      <path fill="transparent" d="M0,0h24v24H0V0z" />
-      <path
-        fill="#000"
-        d="M20.5,5.2l-1.4-1.7C18.9,3.2,18.5,3,18,3H6C5.5,3,5.1,3.2,4.8,3.5L3.5,5.2C3.2,5.6,3,6,3,6.5V19  c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V6.5C21,6,20.8,5.6,20.5,5.2z M12,17.5L6.5,12H10v-2h4v2h3.5L12,17.5z M5.1,5l0.8-1h12l0.9,1  H5.1z"
-      />
-    </svg>
-  );
-
-  const Logo2 = () => (
     <svg className="w-24 h-24 mt-32 pointer-events-none" x="0px" y="0px" viewBox="0 0 24 24">
       <path fill="transparent" d="M0,0h24v24H0V0z" />
       <path
@@ -84,15 +74,10 @@ const ProjectDetail = () => {
 
   const handleUploadButtonClick = () => {
     setShowPopup(false);
-
-    if (filePreviews.length) {
-      setActive(false);
-      setExist(true);
-
-      // filePreviews 배열에 있는 데이터를 fileFinals 배열에 추가하고 비우기
-      setFileFinals(prevFileFinals => [...prevFileFinals, ...filePreviews]);
-      setFilePreviews([]);
-    }
+    setActive(false);
+    setExist(true);
+    setFileFinals(prevFileFinals => [...prevFileFinals, ...filePreviews]);
+    setFilePreviews([]);
   };
 
   const handlePopUpButtonClick = () => {
@@ -214,7 +199,7 @@ const ProjectDetail = () => {
                           <>
                             <Logo />
                             <p className="font-medium font-PretendardVariable text-lg my-20 mb-10">클릭 혹은 여러 파일을 이곳에 드롭하세요!</p>
-                            <p className="mb-5 font-PretendardVariable text-sm">파일당 최대 3MB</p>
+                            <p className="mb-32 font-PretendardVariable text-sm">파일당 최대 3MB</p>
                           </>
                         )}
                         <input type="file" className="file hidden" accept='.png, .jpeg, .pdf' onChange={handleUpload} multiple />
@@ -286,7 +271,7 @@ const ProjectDetail = () => {
                       </>
                     ) : (
                       <>
-                        <Logo2 />
+                        <Logo />
                         <p className="font-medium font-PretendardVariable text-lg my-20 mb-10">클릭 혹은 여러 파일을 이곳에 드롭하세요!</p>
                         <p className="mb-32 font-PretendardVariable text-sm">파일당 최대 3MB</p>
                       </>
