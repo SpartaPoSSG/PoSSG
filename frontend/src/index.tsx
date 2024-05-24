d64137e2e227f0329784ae431056b5ca4188096b
jiheunkim <ziheun54@gmail.com>
2024-05-24 15:36:58 +0900

    Fix: pdf style 관련 경고 없애기

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
