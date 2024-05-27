ad90dd17f1d2705c5a2740b0ab4c8dff776ad6dd
jiheunkim <ziheun54@gmail.com>
2024-05-28 03:12:43 +0900

    Feat: 폴더 존재 여부로 인한 조건문 (예은작업 누락부분 추가)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,11 @@
-import React, { useState, useEffect,FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import ProjectFolder from '../components/ProjectFolder';
 import { MySectors } from '../interfaces/Interfaces';
-import { useNavigate } from 'react-router-dom';
 import { getMyFolder, manageFolder } from '../api/possgAxios';
 import InputForm from '../components/InputForm';
 
 
 const Home = () => {
-    const navigate = useNavigate(); 
     const token = localStorage.getItem('token');
 
     const [folders, setFolders] = useState<MySectors>([
@@ -86,39 +84,41 @@ const Home = () => {
     };
 
 
-      return (
-        <>
-            <div className='comp_content bg-g flex w-screen justify-center self-stretch bg-white text-gray-700'>
-                <div className='flex flex-1 flex-col md:flex-row box-border max-w-screen-xl items-center justify-start px-5 md:px-20 xl:px-10 pt-20 pb-20'>
-                    <div className='flex-1 flex-grow-4 self-start max-w-none prose-lg mx-4 text-gray-700'>
-                        {folders.map(({ name, folders }) => (
-                            <div key={name} className='comp_summary mx-auto md:w-[80%]'>
-                                <div className='flex justify-between items-center pt-12 pb-2 border-b border-gray-500'>
-                                    <p className='text-xl font-PretendardVariable font-semibold ml-3'>{name}</p>
-                                    <InputForm
-                                        value={newFolderNames[name]}
-                                        onChange={(e) => setNewFolderNames(prevNames => ({ ...prevNames, [name]: e.target.value }))}
-                                        onSubmit={(e) => handleMakeFolder(e, name)}
-                                        placeholder="폴더 제목을 입력하세요"
-                                    />
-                                </div>
-                                <div className='grid grid-cols-1 md:grid-cols-3 gap-4 pt-5'>
-                                    {folders.map((folder, index) => (
+    return (
+        <div className='comp_content bg-g flex w-screen justify-center self-stretch bg-white text-gray-700'>
+            <div className='flex flex-1 flex-col md:flex-row box-border max-w-screen-xl items-center justify-start px-5 md:px-20 xl:px-10 pt-20 pb-20'>
+                <div className='flex-1 flex-grow-4 self-start max-w-none prose-lg mx-4 text-gray-700'>
+                    {folders.map(({ name, folders }) => (
+                        <div key={name} className='comp_summary mx-auto md:w-[80%]'>
+                            <div className='flex justify-between items-center pt-12 pb-2 border-b border-gray-500'>
+                                <p className='text-xl font-PretendardVariable font-semibold ml-3'>{name}</p>
+                                <InputForm
+                                    value={newFolderNames[name]}
+                                    onChange={(e) => setNewFolderNames(prevNames => ({ ...prevNames, [name]: e.target.value }))}
+                                    onSubmit={(e) => handleMakeFolder(e, name)}
+                                    placeholder="폴더 제목을 입력하세요"
+                                />
+                            </div>
+                            <div className='grid grid-cols-1 md:grid-cols-3 gap-4 pt-5'>
+                                {folders.length === 0 ? (
+                                    <div className="text-center">폴더가 없습니다.</div>
+                                ) : (
+                                    folders.map((folder, index) => (
                                         <div key={index} className='flex flex-col w-full p-2'>
                                             <ProjectFolder
                                                 sector={name}
-                                                src={'folder.src'}
+                                                src={folder.src}
                                                 title={folder.title}
                                             />
                                         </div>
-                                    ))}
-                                </div>
+                                    ))
+                                )}
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
