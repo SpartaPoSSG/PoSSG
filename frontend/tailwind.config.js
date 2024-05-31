660eb65b3f56cd255b2b8d924dde99d159247b82
jiheunkim <ziheun54@gmail.com>
2024-06-01 01:21:49 +0900

    Feat: 프로젝트 페이지 내 포폴 이동 버튼 생성

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      }
+    },
     fontFamily: {
       'seoleim': ['seolleimcool-bold']
     }
