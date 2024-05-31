18cd57be5f5b39c66607ba7c7859e5e1b6aef7fd
jiheunkim <ziheun54@gmail.com>
2024-05-31 19:40:43 +0900

    Design: Navbar 디자인 수정 #35

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'seoleim': ['seolleimcool-bold']
+    }
   },
   plugins: [
     require('flowbite/plugin'), require("tailwind-scrollbar-hide")
