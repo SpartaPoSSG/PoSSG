a60f152e423dc78d49cfcb0a52c3123648fa94d0
jiheunkim <ziheun54@gmail.com>
2024-06-27 22:56:30 +0900

    Design: 회원가입 디자인 수정

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -132,133 +132,25 @@ const Register = () => {
     }
   };
 
-  //   return (
-  //     <>
-  //       <div className='main-font'>
-  //         <section className="bg-white dark:bg-gray-900">
-  //           <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-  //             <div className="register-box-container w-full bg-white rounded-lg md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-  //               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-  //                 <form className="space-y-4 md:space-y-5" onSubmit={handleSubmit}>
-  //                   <div className='relative'>
-  //                     <input
-  //                       type="email"
-  //                       name="email"
-  //                       id="email"
-  //                       value={signupForm.email}
-  //                       onChange={handleChange}
-  //                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-  //                       placeholder="이메일"
-  //                       required
-  //                     />
-  //                     <button
-  //                       className="absolute right-3 top-4 w-15 bg-blue-900 text-white text-xs font-PretendardVariable font-normal rounded-md py-1 px-2 transition duration-200 ease-in-out cursor-pointer"
-  //                       onClick={handleCheckEmail}>중복확인
-  //                     </button>
-  //                     <p className={`text-gray-500 sm:text-sm ml-2 mt-1`}>
-  //                       {validMessage.emailMessage}
-  //                     </p>
-  //                   </div>
-  //                   <div className='relative'>
-  //                     <input
-  //                       type="text"
-  //                       name="nickname"
-  //                       id="nickname"
-  //                       value={signupForm.nickname}
-  //                       onChange={handleChange}
-  //                       maxLength={10}
-  //                       className={`bg-gray-50 border border-gray-300 text-gray-800 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-  //                       placeholder="닉네임"
-  //                       required
-  //                     />
-  //                     <p className={`text-gray-500 sm:text-sm ml-2 mt-1`}>
-  //                       {validMessage.nicknameMessage}
-  //                     </p>
-  //                   </div>
-  //                   <div>
-  //                     <input
-  //                       type="password"
-  //                       name="password"
-  //                       id="password"
-  //                       value={signupForm.password}
-  //                       onChange={handleChange}
-  //                       placeholder="비밀번호"
-  //                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-  //                       required
-  //                     />
-  //                     <p className="text-red-500 sm:text-sm ml-2 mt-1">
-  //                     {validMessage.passwordMessage}
-  //                     </p>
-  //                   </div>
-  //                   <div>
-  //                     <input
-  //                       type="password"
-  //                       name="checkedPassword"
-  //                       id="checkedPassword"
-  //                       placeholder="비밀번호 확인"
-  //                       value={signupForm.checkedPassword}
-  //                       onChange={handleChange}
-  //                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-  //                       required
-  //                     />
-  //                     <p className="text-red-500 sm:text-sm ml-2 mt-1">
-  //                     {validMessage.checkedPasswordMessage}
-  //                     </p>
-  //                   </div>
-  //                   <br></br>
-  //                   <div className="flex items-start">
-  //                     <div className="flex items-center h-5">
-  //                       <input
-  //                         id="terms"
-  //                         aria-describedby="terms"
-  //                         type="checkbox"
-  //                         className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-  //                         required
-  //                       />
-  //                     </div>
-  //                     <div className="ml-3 text-sm">
-  //                       <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
-  //                         포쓱의 <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="/">이용약관</a>에 동의합니다.</label>
-  //                     </div>
-  //                   </div>
-  //                   <button
-  //                     type="submit"
-  //                     className="w-full flex justify-center rounded-lg bg-blue-900 py-3 px-4 font-PretendardVariable text-lg font-medium leading-tight text-white shadow-md transition duration-200 ease-in-out cursor-pointer mb-2">
-  //                     포쓱 시작하기
-  //                   </button>
-  //                   <p className="mt-10 text-center text-sm text-gray-500">
-  //                     이미 회원가입을 하셨나요?&nbsp;&nbsp;&nbsp;
-  //                     <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">로그인하기</a>
-  //                   </p>
-  //                 </form>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </section>
-  //       </div>
-  //     </>
-  //   );
-  // };
-
-  // export default Register;
 
   return (
     <>
-       <div className='bg-blue-500 w-screen min-h-screen'>
+       <div className='bg-blue-600 w-screen min-h-screen'>
         <section className="dark:bg-gray-900">
-          <div className='flex justify-between pt-20 px-20'>
-            <div className='text-white text-3xl leading-snug flex flex-col justify-center items-center min-h-screen'>
-              <div className='px-10 text-left'>
-                <p className='font-bold text-2xl leading-tight'>걱정하지 마세요, 포트폴리오는 간단합니다.</p>
-                <br /><br />
-                <p className='font-bold leading-tight'>POSSG</p>
-                <br /><br />
-                <p className='font-semibold text-lg leading-tight'>나만의 멋진 포트폴리오를 손쉽게 만들어 보세요.</p>
+          <div className='flex justify-between pt-10 px-20'>
+            <div className='text-white text-3xl leading-snug flex flex-col justify-center min-h-screen'>
+              <div className='px-10'>
+                <p className='font-semibold text-3xl leading-tight'>걱정하지 마세요,<br></br>포트폴리오는 간단합니다.</p><br></br>
+                <p className='font-semibold text-2xl leading-tight'>포트폴리오를 한 번에 쓱!</p>
+                <p className='font-semibold text-2xl'>
+                  <span className='text-3xl font-semibold'>POSSG</span>
+                  과 함께 나만의 멋진 포트폴리오를 만들어 보세요.
+                </p>
               </div>
               <img
-                className="w-96 pl-12 pt-10 mt-20"
+                className="w-80 pl-10 pt-10 mt-20"
                 src={`/img/charactor.png`}
-                alt="포쓱 캐릭터 이미지"
+                alt="possg-charactor"
               />
             </div>
 
@@ -279,7 +171,7 @@ const Register = () => {
                         required
                       />
                       <button
-                        className="absolute right-3 top-10 w-15 bg-main text-white text-xs font-PretendardVariable font-normal rounded-md py-1 px-2 transition duration-200 ease-in-out cursor-pointer"
+                        className="absolute right-3 top-10 w-15 bg-blue-600 text-white text-xs font-PretendardVariable font-normal rounded-md py-1 px-2 transition duration-200 ease-in-out cursor-pointer"
                         onClick={handleCheckEmail}>중복확인
                       </button>
                       <p className={`text-gray-500 sm:text-sm ml-2 mt-1`}>
@@ -350,7 +242,7 @@ const Register = () => {
                     </div>
                     <button
                       type="submit"
-                      className="w-full flex justify-center rounded-lg bg-blue-500 py-3 px-4 text-lg font-semibold leading-tight text-white shadow-md transition duration-200 ease-in-out cursor-pointer mb-2">
+                      className="w-full flex justify-center rounded-lg bg-blue-600 py-3 px-4 text-lg font-semibold leading-tight text-white shadow-md transition duration-200 ease-in-out cursor-pointer mb-2">
                       회원가입
                     </button>
                     <p className="mt-10 text-center text-sm text-gray-500">
